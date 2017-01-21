57799e0d99d2d5fdd77fe87fbddb2f2c204e419b
Vixen <bavoysi@gmail.com>
2017-01-21 09:18:56 +0100

    Base SFML

---
+++ LiquidFighters/LiquidFighters/LiquidFighters.cpp
@@ -0,0 +1,29 @@
+// LiquidFighters.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(840, 420), "LiquidFihters");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+    return 0;
+}
+
